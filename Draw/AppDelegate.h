0ed4de463f2a1fdc9b948327e6a718c7b23f5207
MarineXmh <15957299a@gmail.com>
2016-01-04 10:03:21 +0800

    Initial Commit

---
+++ Draw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Draw
+//
+//  Created by Xu Menghua on 16/1/4.
+//  Copyright © 2016年 Xu Menghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Draw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Draw
+//
+//  Created by Xu Menghua on 16/1/4.
+//  Copyright © 2016年 Xu Menghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
